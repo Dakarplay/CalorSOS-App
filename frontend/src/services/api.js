4a8d4a8e9361428bcd8b54d524902386dacadb8b
Dago Palmera <dagopalmera1@gmail.com>
2025-11-13 18:30:36 -0500

    configurar cliente API con interceptor de autenticación

---
+++ frontend/src/services/api.js
@@ -0,0 +1,16 @@
+// src/services/api.js
+import axios from "axios";
+
+// URL base del backend
+const API = axios.create({
+  baseURL: "http://localhost:8000", // cámbialo según despliegue
+});
+
+// Interceptor para añadir token JWT automáticamente
+API.interceptors.request.use((config) => {
+    const token = localStorage.getItem("token");
+    if (token) config.headers.Authorization = `Bearer ${token}`;
+    return config;
+});
+
+export default API;
