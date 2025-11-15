e70335d41dc287111c308e3ae017caa0b6f0c145
Dago Palmera <dagopalmera1@gmail.com>
2025-11-14 19:43:32 -0500

    feat: creacion de la pagina configuracion

---
+++ frontend/src/pages/Configuracion.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavbarSmart from '../components/ui/NavbarSmart';
+
+export default function Configuracion() {
+    return (
+        <>
+        <NavbarSmart />
+        <div className="page-container">
+            <h1 className="page-title">Configuración</h1>
+            {/* Tu contenido aquí */}
+        </div>
+        </>
+    );
+}
