2b7c76283ea1d3cfdc181624b9086cb39efc5eab
Dago Palmera <dagopalmera1@gmail.com>
2025-11-14 19:43:15 -0500

    feat: creacion de la pagina alertas

---
+++ frontend/src/pages/Alertas.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavbarSmart from '../components/ui/NavbarSmart';
+
+export default function Alertas() {
+    return (
+        <>
+        <NavbarSmart />
+        <div className="page-container">
+            <h1 className="page-title">Alertas</h1>
+            {/* Tu contenido aquí */}
+        </div>
+        </>
+    );
+}
