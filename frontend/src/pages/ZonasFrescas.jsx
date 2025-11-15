d9825c7bbe10564365a4bbbd42d953bec7983ab8
Dago Palmera <dagopalmera1@gmail.com>
2025-11-14 19:47:43 -0500

    feat: creacion pagina para zonas frescas

---
+++ frontend/src/pages/ZonasFrescas.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavbarSmart from '../components/ui/NavbarSmart';
+
+export default function ZonasFrescas() {
+    return (
+        <>
+        <NavbarSmart />
+        <div className="page-container">
+            <h1 className="page-title">Zonas Frescas</h1>
+            {/* Tu contenido aquí */}
+        </div>
+        </>
+    );
+}
