76a9a77d419a737f2b266b15a95ce02299be6dcd
Dago Palmera <dagopalmera1@gmail.com>
2025-11-14 19:35:41 -0500

    feat: Añadir importación de configuración de Leaflet para la funcionalidad de mapas

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { UserProvider } from "./context/UserContext.jsx";
 import { BrowserRouter } from "react-router-dom";
+
 import "./assets/styles/global.css";
+import "./utils/leafletConfig.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
